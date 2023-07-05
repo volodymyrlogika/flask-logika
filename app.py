07437e6900f391174edcbb0221242a4c856f5476
volodymyrlogika <95148713+volodymyrlogika@users.noreply.github.com>
2023-07-05 18:45:52 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from sql_queries import *
 
 app = Flask(__name__) #створюємо Flask-додаток
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/index")
+def index():
+    posts = get_posts()
+    category_list = get_categorys()
+    return render_template("index.html", category_list = category_list, posts=posts)
 
-@app.route("/logika")
-def logika():
-    return render_template("logika.html")
+@app.route("/category/<id>")
+def category(id):
+    posts = get_category_post(id)
+    category = get_category_name(id)[0].upper()
+    category_list = get_categorys()
+    return render_template("category_posts.html", category_list = category_list, category = category, posts=posts)
+
+@app.route("/post/<post_id>")
+def post(post_id):
+    post = get_post(post_id)
+    category_list = get_categorys()
+    return render_template("post.html", category_list = category_list, post=post)
+
+@app.route("/post/new", methods=["POST", "GET"])
+def new_post():
+    category_list = get_categorys()
+    if request.method == "POST":
+        add_post(request.form['category'], request.form['title'], request.form['text'])
+    return render_template("newpost.html", category_list = category_list)
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True  # автоматичне оновлення шаблонів
--- sql_queries.py
+++ sql_queries.py
@@ -1,2 +1,82 @@
 import sqlite3
 
+def open():
+    global conn, cursor
+    conn = sqlite3.connect("blog.db")
+    cursor = conn.cursor()
+
+def close():
+    cursor.close()
+    conn.close()
+
+def add_category(data):
+    open()
+    cursor.execute('''
+    INSERT INTO category (name) VALUES ((?))''', [data] )
+    conn.commit()
+    close()
+
+def add_user(*data):
+    open()
+    cursor.execute('''
+    INSERT INTO users (name, email, password) VALUES ((?), (?), (?))''', [data[0], data[1], data[2]] )
+    conn.commit()
+    close()
+
+def get_user(user_id):
+    open()
+    cursor.execute('''SELECT * from users WHERE id == (?)''', [user_id])
+    user = cursor.fetchone()
+    conn.commit()
+    close()
+    return user
+
+def get_post(post_id):
+    open()
+    cursor.execute('''SELECT * from posts WHERE id == (?)''', [post_id])
+    post = cursor.fetchone()
+    conn.commit()
+    close()
+    return post
+
+
+def get_category_name(id):
+    open()
+    cursor.execute('''SELECT name from category WHERE id == (?)''', [id])
+    category = cursor.fetchone()
+    conn.commit()
+    close()
+    return category
+
+def get_category_post(id):
+    open()
+    cursor.execute('''SELECT * from posts WHERE category_id == (?)''', [id])
+    post = cursor.fetchall()
+    conn.commit()
+    close()
+    return post
+
+def get_posts():
+    open()
+    cursor.execute('''SELECT * from posts''')
+    posts = cursor.fetchall()
+    conn.commit()
+    close()
+    return posts
+
+def get_categorys():
+    open()
+    cursor.execute('''SELECT * from category''')
+    data = cursor.fetchall()
+    conn.commit()
+    close()
+    return data
+
+def add_post(*data):
+    open()
+    cursor.execute('''
+    INSERT INTO posts (category_id, title, text) VALUES ((?), (?), (?))''', [data[0], data[1], data[2]] )
+    conn.commit()
+    close()
+
+
